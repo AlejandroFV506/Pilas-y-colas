138adc0cf092438960c9e4377de2612a27e9972e
ItsLean <lruiz@estudiantec.cr>
2025-03-21 19:48:46 -0600

    Commit ArrayCola

---
+++ ArrayCola/ArrayCola.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayCola
+{
+    public class ArrayCola
+    {
+        private int[] elementos;
+        private int frente;
+        private int final;
+        private int capacidad;
+        private int tamaño;
+
+        public ArrayCola(int capacidad)
+        {
+            this.capacidad = capacidad;
+            elementos = new int[capacidad];
+            frente = 0;
+            final = -1;
+            tamaño = 0;
+        }
+
+        public void Encolar(int valor)
+        {
+            if (tamaño == capacidad)
+            {
+                throw new InvalidOperationException("La cola está llena");
+            }
+
+            final = (final + 1) % capacidad;
+            elementos[final] = valor;
+            tamaño++;
+        }
+
+        public int Desencolar()
+        {
+            if (tamaño == 0)
+            {
+                throw new InvalidOperationException("La cola está vacía");
+            }
+
+            int valor = elementos[frente];
+            frente = (frente + 1) % capacidad;
+            tamaño--;
+            return valor;
+        }
+
+        public int Frente()
+        {
+            if (tamaño == 0)
+            {
+                throw new InvalidOperationException("La cola está vacía");
+            }
+
+            return elementos[frente];
+        }
+
+        public int Tamaño()
+        {
+            return tamaño;
+        }
+
+        public bool EstaVacia()
+        {
+            return tamaño == 0;
+        }
+
+        public bool EstaLlena()
+        {
+            return tamaño == capacidad;
+        }
+
+        public override string ToString()
+        {
+            if (tamaño == 0)
+            {
+                return "[]";
+            }
+
+            string resultado = "[";
+            for (int i = 0; i < tamaño; i++)
+            {
+                int indice = (frente + i) % capacidad;
+                resultado += elementos[indice];
+                if (i < tamaño - 1)
+                {
+                    resultado += ", ";
+                }
+            }
+            resultado += "]";
+            return resultado;
+        }
+    }
+}
+    
+    
+     
+
+
--- ArrayCola/Program.cs
+++ ArrayCola/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ArrayCola;
+
+class Program
+{
+    static void Main()
+    {
+        var cola = new ArrayCola.ArrayCola(5);
+
+        cola.Encolar(1);
+        cola.Encolar(2);
+        cola.Encolar(3);
+
+        Console.WriteLine(cola.ToString()); // Output: [1, 2, 3]
+
+        Console.WriteLine("Desencolar: " + cola.Desencolar()); // Output: Desencolar: 1
+
+        Console.WriteLine(cola.ToString()); // Output: [2, 3]
+
+        cola.Encolar(4);
+        cola.Encolar(5);
+
+        Console.WriteLine(cola.ToString()); // Output: [2, 3, 4, 5]
+
+        Console.WriteLine("Frente: " + cola.Frente()); // Output: Frente: 2
+
+        Console.WriteLine("Tamaño: " + cola.Tamaño()); // Output: Tamaño: 4
+
+        Console.WriteLine("¿Está vacía?: " + cola.EstaVacia()); // Output: ¿Está vacía?: False
+        Console.WriteLine("¿Está llena?: " + cola.EstaLlena()); // Output: ¿Está llena?: False
+    }
+}
